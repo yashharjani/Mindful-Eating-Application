f4c4f95e7414a0bf918853f40206b5cdc516783f
Yash Harjani <yashharjani2001@gmail.com>
2025-12-04 22:09:55 -0400

    Final deployment

--- frontend/utils/config.js
+++ frontend/utils/config.js
@@ -1,3 +1,3 @@
-// const BASE_URL = 'https://persuasive.research.cs.dal.ca/mindfuleating/';
-const BASE_URL = 'http://192.168.2.16:8080';
+const BASE_URL = 'https://persuasive.research.cs.dal.ca/mindfuleating';
+// const BASE_URL = 'http://192.168.2.16:8080';
 export default BASE_URL;
